fd125ebeab1bdb6b28d75f7c4433c5c2b657e018
cshel482 <csheldon482@gmail.com>
2023-11-05 14:27:22 -0600

    Added the Display Attribute for the course page to split words up by spaces

--- Models/Course.cs
+++ Models/Course.cs
@@ -5,11 +5,18 @@ namespace CourseCatalog.Models
     public class Course
     {
         public int Id { get; set; }
+        
+        [Display(Name = "Course Name")]
         public string? CourseName { get; set; }
+        
+        [Display(Name = "Course Description")]
         public string? CourseDescription { get; set; }
+        
+        [Display(Name = "Room Number")]
         public int RoomNumber { get; set; }
 
         [DataType(DataType.Time)]
+        [Display(Name = "Start Time")]
         public TimeOnly StartTime { get; set; }
     }
 }
